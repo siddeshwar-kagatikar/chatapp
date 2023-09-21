1072ad085a7f945e856f0def277036ab84ade169
siddeshwar-kagatikar <siddeshwar2004@gmail.com>
2023-09-21 14:54:10 +0530

    addded frontend link

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const io = new Server(server,{
     cors:{
-        origin: "http://localhost:3000",
+        origin: "https://chatapp-frontend-51aq.onrender.com",
         methods: ["GET","POST"],
     },
 });
